689b2bc74364e5cdbb5c8ba99979a7facaf6c5de
Rustam Barlybaev <bargus@rambler.ru>
2019-08-01 19:50:04 +0300

    Second commit. Trying to create function getStocks for returning currencies.

--- static/main.js
+++ static/main.js
@@ -7,7 +7,7 @@ class Profile {
 
   createUser(callback){
     return ApiConnector.createUser({ username: this.username, 
-                                     name:this.name, 
+                                     name: this.name, 
                                      password: this.password 
                                     }, callback);
   }
@@ -21,9 +21,16 @@ class Profile {
   addMoney({ currency, amount }, callback) {
     return ApiConnector.addMoney({ currency, amount }, (err, data) => {
         console.log(`Adding ${amount} of ${currency} to ${this.username}`);
+        //console.log(err);
+        //console.log(data);
         callback(err, data);
     });
   }
+
+  convertMoney(){
+
+  }
+
 }
 
 function main() {
@@ -50,10 +57,37 @@ function main() {
                             console.error(`Error during authorizing user ${user1.username}`);
                           } else {
                             console.log(`Authorizing user ${user1.username}`);
+                            let money = {currency: 'EUR', 
+                                         amount: 3500
+                                        };
+                            user1.addMoney(money, (err, data) => {
+                                            if(err){
+                                              console.error(`Error during adding money to user ${user1.username}`);
+                                            } else {
+                                              console.log(`Added ${money.amount} of ${money.currency} to user ${user1.username}`);
+                                            }
+                                          });
                           }
                         });
     }
   });
 }
 
-main();
+function getStocks (){
+  return ApiConnector.getStocks((err, data) => {
+    if(err){
+      console.error('Error getting stocks: '+ err);
+    } else {
+      console.log('Got stocks: ');
+      console.log(data);
+      return data;
+    }
+  });
+}
+
+//let a = getStocks();
+//console.log(a);
+console.log(getStocks());
+
+
+//main();
